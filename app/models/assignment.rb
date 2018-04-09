f36b3aa8f27222565bdf08e39ec5ff79ba0ac043
Souleiman Benhida <souleben@gmail.com>
2018-04-09 12:19:43 -0400

    added more touches, going to change mysql version and add forum

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,16 @@
 class Assignment < ApplicationRecord
 	belongs_to :classroom
 	has_many :questions
-	has_many :submissions
+	has_many :submissions, :through => :questions
+
+	def grade(student)
+		student_submissions = submissions.find_all { |submission| submission.student_id == student.id }
+
+		if student_submissions.count==0 
+			return ""
+		end
+		scores = student_submissions.map { |s| (s.answer == s.question.correct_answer) ? 1 : 0 }
+
+		return scores.sum.to_s + "/" + student_submissions.count.to_s
+	end
 end
